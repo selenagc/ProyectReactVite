f87571fb877365804e3ba38cce0534cd8b2218d1
Selenagc <prueba@correo.com>
2025-10-07 14:49:29 -0400

    Creando plantilla

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Tareas from './views/Tareas.jsx';
+import Categorias from './views/Categorias.jsx';
+import Etiquetas from './views/Etiquetas.jsx';
 
 function App() {
-  return <h1>Hola Mundo</h1>;
+  return (
+    <Router>
+      <div className="d-flex flex-column vh-100">
+        <Header />
+
+        <div className="flex-grow-1 p-3">
+          <Routes>
+            <Route path="/tareas" element={<Tareas />} />
+            <Route path="/categorias" element={<Categorias />} />
+            <Route path="/etiquetas" element={<Etiquetas />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
-
-
---
+++ src/views/Tareas.jsx
@@ -0,0 +1,5 @@
+function Tareas() {
+  return <h2>Página de Tareas</h2>;
+}
+
+export default Tareas;
