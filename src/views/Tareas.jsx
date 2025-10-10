cd2cc6633462b42cb066705297b9b6a32fb28ece
Selenagc <prueba@correo.com>
2025-10-09 20:05:06 -0400

    PB-6

--- src/views/Tareas.jsx
+++ src/views/Tareas.jsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+import { getAll } from "../services/tarea_service.jsx";
+
 function Tareas() {
-  return <h2>Página de Tareas</h2>;
+  const [tareas, setTareas] = useState([]);
+
+  useEffect(() => {
+    const cargarTareas = async () => {
+      const data = await getAll();
+      console.log("Tareas obtenidas:", data);
+      setTareas(data);
+    };
+    cargarTareas();
+  }, []);
+
+
 }
 
 export default Tareas;
